99a042317857db150916ea16d50e0bc9b6bbbf03
Xincan <cecilia.fung@foxmail.com>
2023-02-07 11:24:31 +0900

    modifying

--- best_config_0207.sh
+++ best_config_0207.sh
@@ -2,14 +2,19 @@
 # SAVE_ID=$5
 # Best Configuration for RotatE
 #
-nohup bash run.sh train RotatE FB15k 0 0 1024 256 1000 24.0 1.0 0.0001 150000 16 -de &
-nohup bash run.sh train RotatE FB15k-237 0 0 1024 256 1000 9.0 1.0 0.00005 100000 16 -de &
-nohup bash run.sh train RotatE wn18 0 0 512 1024 500 12.0 0.5 0.0001 80000 8 -de &
-nohup bash run.sh train RotatE wn18rr 0 0 512 1024 500 6.0 0.5 0.00005 80000 8 -de &
+# nohup bash run.sh train RotatE FB15k 2 0 1024 256 1000 24.0 1.0 0.0001 150000 16 -de &
+# nohup bash run.sh train RotatE FB15k-237 2 0 1024 256 1000 9.0 1.0 0.00005 100000 16 -de &
+# nohup bash run.sh train RotatE wn18 2 0 512 1024 500 12.0 0.5 0.0001 80000 8 -de &
+# nohup bash run.sh train RotatE wn18rr 2 0 512 1024 500 6.0 0.5 0.00005 80000 8 -de &
 nohup bash run.sh train RotatE countries_S1 0 0 512 64 1000 0.1 1.0 0.000002 40000 8 -de --countries &
 nohup bash run.sh train RotatE countries_S2 0 0 512 64 1000 0.1 1.0 0.000002 40000 8 -de --countries &
 nohup bash run.sh train RotatE countries_S3 0 0 512 64 1000 0.1 1.0 0.000002 40000 8 -de --countries &
 nohup bash run.sh train RotatE YAGO3-10 0 0 1024 400 500 24.0 1.0 0.0002 100000 4 -de &
+
+# nohup bash run.sh train RotatE FB15k 3 s0 1024 256 1000 24.0 1.0 0.0001 150000 16 -de --ss &
+# nohup bash run.sh train RotatE FB15k-237 3 s0 1024 256 1000 9.0 1.0 0.00005 100000 16 -de --ss &
+# nohup bash run.sh train RotatE wn18 3 s0 512 1024 500 12.0 0.5 0.0001 80000 8 -de --ss &
+# nohup bash run.sh train RotatE wn18rr 3 s0 512 1024 500 6.0 0.5 0.00005 80000 8 -de --ss &
 #
 # Best Configuration for pRotatE
 #
@@ -33,18 +38,18 @@ nohup bash run.sh train TransE countries_S3 0 0 512 64 1000 0.1 1.0 0.000002 400
 #
 # Best Configuration for ComplEx
 # 
-nohup bash run.sh train ComplEx FB15k 0 0 1024 256 1000 500.0 1.0 0.001 150000 16 -de -dr -r 0.000002 &
-nohup bash run.sh train ComplEx FB15k-237 0 0 1024 256 1000 200.0 1.0 0.001 100000 16 -de -dr -r 0.00001 &
-nohup bash run.sh train ComplEx wn18 0 0 512 1024 500 200.0 1.0 0.001 80000 8 -de -dr -r 0.00001 &
-nohup bash run.sh train ComplEx wn18rr 0 0 512 1024 500 200.0 1.0 0.002 80000 8 -de -dr -r 0.000005 &
+# nohup bash run.sh train ComplEx FB15k 0 0 1024 256 1000 500.0 1.0 0.001 150000 16 -de -dr -r 0.000002 &
+# nohup bash run.sh train ComplEx FB15k-237 0 0 1024 256 1000 200.0 1.0 0.001 100000 16 -de -dr -r 0.00001 &
+# nohup bash run.sh train ComplEx wn18 0 0 512 1024 500 200.0 1.0 0.001 80000 8 -de -dr -r 0.00001 &
+# nohup bash run.sh train ComplEx wn18rr 0 0 512 1024 500 200.0 1.0 0.002 80000 8 -de -dr -r 0.000005 &
 nohup bash run.sh train ComplEx countries_S1 0 0 512 64 1000 1.0 1.0 0.000002 40000 8 -de -dr -r 0.0005 --countries &
 nohup bash run.sh train ComplEx countries_S2 0 0 512 64 1000 1.0 1.0 0.000002 40000 8 -de -dr -r 0.0005 --countries &
 nohup bash run.sh train ComplEx countries_S3 0 0 512 64 1000 1.0 1.0 0.000002 40000 8 -de -dr -r 0.0005 --countries &
 
-nohup bash run.sh train ComplEx FB15k 0 s0 1024 256 1000 500.0 1.0 0.001 150000 16 -de -dr -r 0.000002 --ss &
-nohup bash run.sh train ComplEx FB15k-237 0 s0 1024 256 1000 200.0 1.0 0.001 100000 16 -de -dr -r 0.00001 --ss &
-nohup bash run.sh train ComplEx wn18 0 s0 512 1024 500 200.0 1.0 0.001 80000 8 -de -dr -r 0.00001 --ss &
-nohup bash run.sh train ComplEx wn18rr 0 s0 512 1024 500 200.0 1.0 0.002 80000 8 -de -dr -r 0.000005 --ss &
+# nohup bash run.sh train ComplEx FB15k 1 s0 1024 256 1000 500.0 1.0 0.001 150000 16 -de -dr -r 0.000002 --ss &
+# nohup bash run.sh train ComplEx FB15k-237 1 s0 1024 256 1000 200.0 1.0 0.001 100000 16 -de -dr -r 0.00001 --ss &
+# nohup bash run.sh train ComplEx wn18 1 s0 512 1024 500 200.0 1.0 0.001 80000 8 -de -dr -r 0.00001 --ss &
+# nohup bash run.sh train ComplEx wn18rr 1 s0 512 1024 500 200.0 1.0 0.002 80000 8 -de -dr -r 0.000005 --ss &
 #
 # Best Configuration for DistMult
 # 
